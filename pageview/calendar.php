f394f5b9cd515acd2ca532eb1e3ef0494b1098f8
Frida Fernhede <fernhedefrida@gmail.com>
2023-12-19 13:14:03 +0100

    Added date range to booked dates display
    now dates between arrival and departure dates are also red, which is good to show if people stay more than one night

--- pageview/calendar.php
+++ pageview/calendar.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/../hotelFunctions.php';
 
+
 $calendar = new Calendar();
 $calendar->create();
 ?>
@@ -14,14 +15,17 @@ $calendar->create();
                 <?php foreach ($calendar->getWeek() as $week) : ?>
                     <tr>
                         <?php foreach ($week as $day) :
+                            $date = "2024-01-" . str_pad($day, 2, "0", STR_PAD_LEFT); //för att få rätt format på datumet
                             $db = connect($dbName);
-                            $statement = $db->prepare("select * from bookings where (arrival = :day or departure = :day) and room_number = 1");
-                            $statement->bindValue(':day', $day);
+                            $statement = $db->prepare("select * from bookings where (arrival = :date or departure = :date or :date between arrival and departure )and room_number = :roomNumber");
+
+                            $statement->bindValue(':date', $date);
+                            $statement->bindValue(':roomNumber', $roomNumber);
                             $statement->execute();
                             $bookings = $statement->fetchAll();
                             $class = count($bookings) > 0 ? 'booked' : '';
                         ?>
-                            <td class="calendarDay <?php echo $class ?>" id="<?php echo $day; ?>" data="<?php echo $day; ?> ">
+                            <td class="calendarDay <?php echo $class ?>" data="<?php echo date("2024-01-" . $day); ?>" data="<?php echo $day; ?> ">
                                 <?php echo $day; ?>
                             </td>
                         <?php endforeach; ?>
