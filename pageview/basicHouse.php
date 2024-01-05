7f78cbfbb5102c2953dfdcf76138b039fd01c29a
Frida Fernhede <fernhedefrida@gmail.com>
2024-01-05 11:16:10 +0100

    yesterday's work, did some styling

--- pageview/basicHouse.php
+++ pageview/basicHouse.php
@@ -1,29 +1,30 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/header.php'; ?>
+require __DIR__ . '/header.php';
+require __DIR__ . '/navbar.php';
+?>
 
-<body>
+<img src="" alt="">
+<div class="houseInformationContainer">
+    <div class="textWrap">
+        <h1>Basic House</h1>
+        <p>Basic House is a house that is basic.</p>
+    </div>
+    <img class="img" src="/Media/House1.png" alt="the basic house">
+</div>
+<div class="imgSlideshow">
     <img src="" alt="">
-    <div class="houseInformationContainer">
-        <div class="textWrap">
-            <h1>Basic House</h1>
-            <p>Basic House is a house that is basic.</p>
-        </div>
-        <img class="img" src="/Media/House1.png" alt="the basic house">
-    </div>
-    <div class="imgSlideshow">
-        <img src="" alt="">
-        <img src="" alt="">
-    </div>
-    <form action="" method="post">
-        <h1>$10/night</h1>
-        <div>
+    <img src="" alt="">
+</div>
+<form action="" method="post">
+    <h1>$10/night</h1>
+    <div>
 
-            <?php
-            $roomNumber = 1;
-            require __DIR__ . '/calendarFunctions.php';
-            require __DIR__ . '/calendar.php';
-            require __DIR__ . '/formFunctions.php';
-            ?>
-            <?php
-            require __DIR__ . '/footer.php';
+        <?php
+        $roomNumber = 1;
+        require __DIR__ . '/calendarFunctions.php';
+        require __DIR__ . '/calendar.php';
+        require __DIR__ . '/formFunctions.php';
+        ?>
+        <?php
+        require __DIR__ . '/footer.php';
