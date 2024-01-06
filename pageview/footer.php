52d878612031b7a2a06182c0bbbab2d4e47e669b
Frida Fernhede <fernhedefrida@gmail.com>
2024-01-06 17:09:26 +0100

    updated footer and review-section

--- pageview/footer.php
+++ pageview/footer.php
@@ -1,12 +1,62 @@
 <div class="footer">
+    <div class="newsletterContainer">
+        <div class="newsletterInput">
+            <h1> Join our newsletter</h1>
+            <small>Get updates on our events, offers and more!</small>
+        </div>
+        <div class="newsletterButton">
+            <input type="text" placeholder="Enter your email">
+            <button>Subscribe</button>
+        </div>
+    </div>
     <div class="footerContainer">
         <div class="footerElements">
-            <h1>HOME</h1>
-            <h1>ABOUT</h1>
-            <img src="/Media/vector.svg" alt="">
-            <h1>BOOK TRIPS</h1>
-            <h1>EVENTS</h1>
+            <div class="footerHeader">
+                <h1>HOME</h1>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+            </div>
+            <div class="footerHeader">
+                <h1>ABOUT</h1>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+            </div>
+            <div class="footerHeader">
+                <h1>EVENTS</h1>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+            </div>
+            <div class="footerHeader">
+                <h1>CONTACT</h1>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+            </div>
+            <div class="footerHeader">
+                <h1>POLICY</h1>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+                <p>x</p>
+            </div>
         </div>
+    </div>
+    <div class="footerLegal">
+        <p>© 2021 STUUGA. All rights reserved.</p>
+    </div>
+    <div class="footerSocial">
+        <img src="/Media/facebook.svg" alt="">
+        <img src="/Media/instagram.svg" alt="">
+        <img src="/Media/twitter.svg" alt="">
     </div>
 </div>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
